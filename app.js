06b64cfea60a9a073e4593ae7e93dc8b7eb8cd86
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-20 18:16:30 +1200

    Created routes folder

--- app.js
+++ app.js
@@ -55,10 +55,16 @@ app.post('/goodbye', (req, res) => {
   res.redirect('/hello')
 })
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
 app.use((err, req, res, next) => {
-  res.locals.err = err
+  res.locals.error = err
   res.status(err.status)
-  res.render('error', err)
+  res.render('error')
 })
 
 app.listen(3000, () => {
