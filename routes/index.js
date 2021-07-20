de6adff2a01ea77f175b4b09467e3f105ddb797c
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-20 18:22:29 +1200

    Router file is set-up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   const name = req.cookies.username 
   if (name) {
     res.render('index', { name })
@@ -10,14 +10,14 @@ app.get('/', (req, res) => {
   }
 })
 
-app.get('/cards', (req, res) => {
+router.get('/cards', (req, res) => {
   res.render('card', { 
     prompt: "Who is buriednin Grant's tomb?",
     hint: "Think about whose tomb it is"
   })
 })
 
-app.get('/hello', (req, res) => {
+router.get('/hello', (req, res) => {
   const name = req.cookies.username 
   if (name) {
     res.redirect('/')
@@ -26,12 +26,14 @@ app.get('/hello', (req, res) => {
   }
 })
 
-app.post('/hello', (req, res) => {
+router.post('/hello', (req, res) => {
   res.cookie('username', req.body.username)
   res.redirect('/')
 })
 
-app.post('/goodbye', (req, res) => {
+router.post('/goodbye', (req, res) => {
   res.clearCookie('username')
   res.redirect('/hello')
 })
+
+module.exports = router
