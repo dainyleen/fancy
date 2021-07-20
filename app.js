c0b7a15acba1d7a28b6626d92120c46e8246e56d
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-20 21:26:08 +1200

    Added port environment

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use((err, req, res, next) => {
   res.render('error')
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
   console.log('The application is running on localhost:3000')
 })
