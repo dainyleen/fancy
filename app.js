a516b2d9acec42e955cc71b00b996cbb0ea32494
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-25 17:47:31 +1200

    Imported static files from treehouse

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use('/static', express.static('public'));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-const mainRoutes = require('./routes')
-const cardRoutes = require('./routes/cards')
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.use(mainRoutes)
-app.use('/cards', cardRoutes)
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.use((err, req, res, next) => {
-  res.locals.error = err
-  res.status(err.status)
-  res.render('error')
-})
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log('The application is running on localhost:3000')
-})
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
